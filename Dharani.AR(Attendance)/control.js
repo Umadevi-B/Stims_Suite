381a0eae0f192d091fdadcf6cf433f69ac508c7e
Umadevi-B <umadevirbs@gmail.com>
2017-03-07 19:18:48 +0530

    this is my new file

--- Dharani.AR(Attendance)/control.js
+++ Dharani.AR(Attendance)/control.js
@@ -6,106 +6,106 @@ $scope.details = [
 "Reg_No" : "13IT001",
 "Name" : "Dhanushya.P",
 "Department" : "B.TECH_IT",
-"Photo" : "Photos/1.jpg",
-"color" :"#00c2cc"
+"Photo" : "Photos/boy.png",
+"color" :"#cdcdc0"
 },
 {
 "Reg_No" : "13IT002",
 "Name" : "Dharani.AR",
 "Department" : "B.TECH_IT",
-"Photo" : "Photos/2.jpg",
-"color": "#d98cb3"
+"Photo" : "Photos/girl.png",
+"color":  "#DDBC95"
 },
 {
 "Reg_No" : "13IT003",
 "Name" : "Dharani.P",
 "Department" : "B.TECH_IT",
 "Photo" : "Photos/3.jpg",
-"color" :"#00c2cc"
+"color" : "#cdcdc0"
 },
 {
 "Reg_No" : "13IT004",
 "Name" : "Dhivya.S",
 "Department" : "B.TECH_IT",
 "Photo" : "Photos/4.jpg",
-"color": "#d98cb3"
+"color": "#DDBC95"
 },
 {
 "Reg_No" : "13IT005",
 "Name" : "Guga Priya",
 "Department" : "B.TECH_IT",
 "Photo" : "Photos/5.jpg",
-"color" :"#00c2cc"
+"color" : "#cdcdc0"
 },
 {
 "Reg_No" : "13IT006",
 "Name" : "Hema latha",
 "Department" : "B.TECH_IT",
 "Photo" : "Photos/6.jpg",
-"color": "#d98cb3"
+"color":  "#DDBC95"
 },
 {
 "Reg_No" : "13IT007",
 "Name" : "V.Jamuna",
 "Department" : "B.TECH_IT",
 "Photo" : "Photos/6.jpg",
-"color": "#00c2cc"
+"color": "#cdcdc0"
 },
 {
 "Reg_No" : "13IT008",
 "Name" : "Jeyamani",
 "Department" : "B.TECH_IT",
 "Photo" : "Photos/6.jpg",
-"color": "#d98cb3"
+"color": "#DDBC95"
 },
 {
 "Reg_No" : "13IT009",
 "Name" : "kalaivani",
 "Department" : "B.TECH_IT",
 "Photo" : "Photos/6.jpg",
-"color": "#00c2cc"
+"color": "#cdcdc0"
 },
 {
 "Reg_No" : "13IT010",
 "Name" : "kaviya",
 "Department" : "B.TECH_IT",
 "Photo" : "Photos/6.jpg",
-"color": "#d98cb3"
+"color": "#DDBC95"
 },
 {
 "Reg_No" : "13IT011",
 "Name" : "V.kiruthiga",
 "Department" : "B.TECH_IT",
 "Photo" : "Photos/6.jpg",
-"color": "#00c2cc"
+"color":"#cdcdc0"
 },
 {
 "Reg_No" : "13IT012",
 "Name" : "H.Kirthiga",
 "Department" : "B.TECH_IT",
 "Photo" : "Photos/6.jpg",
-"color": "#d98cb3"
+"color": "#DDBC95"
 },
 {
 "Reg_No" : "13IT013",
 "Name" : "Madhumitha",
 "Department" : "B.TECH_IT",
 "Photo" : "Photos/6.jpg",
-"color": "#00c2cc"
+"color": "#cdcdc0"
 },
 {
 "Reg_No" : "13IT014",
 "Name" : "Manimegalai",
 "Department" : "B.TECH_IT",
 "Photo" : "Photos/6.jpg",
-"color": "#d98cb3"
+"color": "#DDBC95"
 },
 {
 "Reg_No" : "13IT015",
 "Name" : "Mekala",
 "Department" : "B.TECH_IT",
 "Photo" : "Photos/6.jpg",
-"color":"#00c2cc"
+"color": "#cdcdc0"
 },
 ];
 }]);
